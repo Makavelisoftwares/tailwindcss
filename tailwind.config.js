6e1bd8201ba8b09c1e860e6d5862b3a895a47cf2
kavelicodes <makavelisoftwares@gmail.com>
2023-04-29 15:31:05 +0300

    finished

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        background:'#f4f4f4'
+      }
+    },
+  },
+  plugins: [],
+}
+
